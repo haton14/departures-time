605387fdbe61378818a84bc9ab53f115ca563cb5
haton14 <iioka.takumi.quena@gmail.com>
2022-07-19 23:38:22 +0900

    m単位で扱う

--- departures-time-api/domain/vo/distance_test.go
+++ departures-time-api/domain/vo/distance_test.go
@@ -9,26 +9,26 @@ import (
 
 func TestNewDistanceAndValue(t *testing.T) {
 	tests := map[string]struct {
-		arg      float64
-		expected float64
+		arg      int
+		expected int
 		hasErr   error
 	}{
 		"[正常]:0": {
-			arg:      0.0,
-			expected: 0.0,
+			arg:      0,
+			expected: 0,
 			hasErr:   nil,
 		},
-		"[正常]:20": {
-			arg:      20.0,
-			expected: 20.0,
+		"[正常]:100000": {
+			arg:      100000,
+			expected: 100000,
 			hasErr:   nil,
 		},
 		"[エラー]:-1": {
-			arg:    -1.0,
+			arg:    -1,
 			hasErr: vo.ErrMinRange,
 		},
-		"[エラー]:21": {
-			arg:    21.0,
+		"[エラー]:100001": {
+			arg:    100001,
 			hasErr: vo.ErrMaxRange,
 		},
 	}
@@ -36,45 +36,6 @@ func TestNewDistanceAndValue(t *testing.T) {
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
 			actual, err := vo.NewDistance(tt.arg)
-			if tt.hasErr != nil {
-				assert.ErrorIs(t, err, tt.hasErr)
-			} else {
-				assert.NoError(t, err)
-				assert.Equal(t, tt.expected, actual.Value())
-			}
-		})
-	}
-}
-
-func TestNewDistanceForMeterAndValue(t *testing.T) {
-	tests := map[string]struct {
-		arg      int
-		expected float64
-		hasErr   error
-	}{
-		"[正常]:0": {
-			arg:      0,
-			expected: 0.0,
-			hasErr:   nil,
-		},
-		"[正常]:20": {
-			arg:      20000,
-			expected: 20.0,
-			hasErr:   nil,
-		},
-		"[エラー]:-1": {
-			arg:    -1,
-			hasErr: vo.ErrMinRange,
-		},
-		"[エラー]:21": {
-			arg:    20001,
-			hasErr: vo.ErrMaxRange,
-		},
-	}
-
-	for name, tt := range tests {
-		t.Run(name, func(t *testing.T) {
-			actual, err := vo.NewDistanceForMeter(tt.arg)
 			if tt.hasErr != nil {
 				assert.ErrorIs(t, err, tt.hasErr)
 			} else {
