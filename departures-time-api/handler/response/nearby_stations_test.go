30de82f2e0879f84698fa7e4656d43167abe1ad6
haton14 <iioka.takumi.quena@gmail.com>
2022-07-24 01:58:47 +0900

    距離順に並べるようにする

--- departures-time-api/handler/response/nearby_stations_test.go
+++ departures-time-api/handler/response/nearby_stations_test.go
@@ -10,11 +10,13 @@ import (
 
 func TestNewNearbyStationsGet(t *testing.T) {
 	arg := []model.NearbyStation{
+		{Code: "22567", Name: "大森海岸", Longitude: 139.73544, Latitude: 35.587576, Distance: 879},
 		{Code: "22566", Name: "大森(東京都)", Longitude: 139.728079, Latitude: 35.588903, Distance: 351},
 	}
 	expected := response.NearbyStationsGet{
 		Stations: []response.NearbyStation{
 			{Code: "22566", Name: "大森(東京都)", Distance: 351},
+			{Code: "22567", Name: "大森海岸", Distance: 879},
 		},
 	}
 	actual := response.NewNearbyStationsGet(arg)
