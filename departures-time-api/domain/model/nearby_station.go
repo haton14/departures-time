fe34b03819f2bc72d790d800230eafd0be8a1aab
haton14 <iioka.takumi.quena@gmail.com>
2022-07-23 23:55:35 +0900

    最寄駅のUT追加

--- departures-time-api/domain/model/nearby_station.go
+++ departures-time-api/domain/model/nearby_station.go
@@ -21,6 +21,7 @@ func (n NearbyStation) WithinRange(rangeDistance vo.Distance) bool {
 	return n.Distance.Value() <= rangeDistance.Value()
 }
 
+// 駅と指定した座標の距離を求める
 func (n NearbyStation) Difference(lo vo.Longitude, la vo.Latitude) float64 {
 	loDifference := n.Longitude.Value() - lo.Value()
 	laDifference := n.Latitude.Value() - la.Value()
