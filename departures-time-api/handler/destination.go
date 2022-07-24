29f3504e58dd977687b108ab8e1aaf4b76fa0fd8
haton14 <iioka.takumi.quena@gmail.com>
2022-07-24 12:11:57 +0900

    目的地検索で結果が0件の場合は404応答する

--- departures-time-api/handler/destination.go
+++ departures-time-api/handler/destination.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"errors"
 	"log"
 	"net/http"
 
+	"github.com/haton14/departures-time/departures-time-api/domain/vo"
 	"github.com/haton14/departures-time/departures-time-api/handler/request"
 	"github.com/haton14/departures-time/departures-time-api/handler/response"
 	"github.com/haton14/departures-time/departures-time-api/usecase"
@@ -25,9 +27,11 @@ func (d Destination) Get(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, ErrorResponse{Message: "リクエストが不正"})
 	}
 	result, err := d.destinationUsecase.GetByName(req.Name)
-	if err != nil {
+	if err != nil && !errors.Is(err, vo.ErrNotFound) {
 		log.Printf("[Error] NearbyStations.GetByCoordinateAndDistance(): %s", err)
 		return c.JSON(http.StatusInternalServerError, ErrorResponse{Message: "サーバ内のエラー"})
+	} else if errors.Is(err, vo.ErrNotFound) {
+		return c.JSON(http.StatusNotFound, ErrorResponse{Message: "検索結果が0件"})
 	}
 	res := response.NewDestinationGet(result)
 	return c.JSON(http.StatusOK, res)
